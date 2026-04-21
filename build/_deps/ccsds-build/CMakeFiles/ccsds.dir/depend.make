c4ee06c5999e8f69cf76f6a35cba926b832d655c
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2026-04-22 06:20:36 +0800

    remove build

--- build/_deps/ccsds-build/CMakeFiles/ccsds.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ccsds.
-# This may be replaced when dependencies are built.
--- build/_deps/ccsds-build/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/astrobyte/Repos/GhostLayer/build/_deps/ccsds-src/examples
-# Build directory: /Users/astrobyte/Repos/GhostLayer/build/_deps/ccsds-build/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
